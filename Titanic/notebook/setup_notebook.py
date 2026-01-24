7ee4f03c385ca746528a2d0c39f2ef6533fcdc0a
albertoakel <alberto.akel@gmail.com>
2026-01-24 17:16:53 -0300

    notebook titanic

--- Titanic/notebook/setup_notebook.py
+++
@@ -1,7 +0,0 @@
-#setub_notebook.py
-import sys, os
-
-def setup_path():
-    project_root = os.path.abspath(os.path.join(os.getcwd(), '..'))
-    if project_root not in sys.path:
-        sys.path.append(project_root)
